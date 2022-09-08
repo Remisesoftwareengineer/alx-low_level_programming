cc6857b99f99391a40f2cfb3dcb59485c5672d9d
Remise Ogenrwot <remiseogenrwot639@gmail.com>
2022-09-08 14:26:11 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  * Description: using the main function
  * this program prints exactly and that piece of art is useful\" - Dora Korpar, 2015-10-19
  * Return: 1
- * */
+ */
 int main(void)
 {
 	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);

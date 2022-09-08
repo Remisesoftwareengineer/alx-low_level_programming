dd8b39dfe038bca2a04f6008a676aee1bf1cb6b2
Remise Ogenrwot <remiseogenrwot639@gmail.com>
2022-09-08 10:14:31 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #!/bin/bash
-
-
-
 #include<stdio.h>
-/*
+/**
  *C program: it puts out Programming is like building a multilingual puzzle
  *Description: It uses puts to print out word instead of prinf function
  *return: 0
  */
 int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

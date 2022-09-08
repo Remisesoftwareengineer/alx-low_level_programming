d69688dc37a8e436b75d570ee6ace21bf37dbb66
Remise Ogenrwot <remiseogenrwot639@gmail.com>
2022-09-08 14:19:24 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 /**
- * main-print strings using printf function
+ * main-Entry point
  * Description: using the main function
  * this program prints exactly and that piece of art is useful\" - Dora Korpar, 2015-10-19
- * Return: 0
+ * Return: 1
  * */
 int main(void)
 {
-	write("and that piece of art is useful""\" - Dora Korpar, 2015-10-19\n");
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }

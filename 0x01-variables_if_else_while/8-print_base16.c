20737ecfd33fd943596c2754d528895bb8014447
Remise Ogenrwot <remiseogenrwot639@gmail.com>
2022-09-09 15:13:56 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,13 +6,12 @@
  */
 int main(void)
 {
-	int i;
-	char alphabet;
+	char base_16;
 
-	for (i = 0; i < 10; i++)
-	putchar(i);
-	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
-	putchar(alphabet);
+	for (base_16 = 48; base_16 <= 57; base_16++)
+	putchar(base_16);
+	for (base_16 = 97; base_16 <= 102; base_16++)
+	putchar(base_16);
 	putchar('\n');
 	return (0);
 }

5034f165d4c64ced931b00e9fb90bd8c8818a01e
Remise Ogenrwot <remiseogenrwot639@gmail.com>
2022-09-10 08:42:36 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,14 +8,12 @@
 int main(void)
 {
 	int n1 = 0, n2, n3;
- 
 	while (n1 <= 9)
 	{
 		n2 = 0;
 		while (n2 <= 0)
 		{
 			n3 = 0;
-
 			while (n3 <= 9)
 			{
 				if (n1 != n2 && n1 < n2 && n2 != n3 && n2 < n3)

9477147de73a112a483558c557784c90dd23a523
Remise Ogenrwot <remiseogenrwot639@gmail.com>
2022-09-10 07:47:46 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int number;
 
-	for (number = 0; number < 100; number++)
+	for (number = 0; number < 90; number++)
 	{
 		putchar((number / 10) + '0');
 		putchar((number % 10) + '0');
-		if (number != 99)
+		if (number != 89)
 		{
 			putchar(',');
 			putchar(' ');

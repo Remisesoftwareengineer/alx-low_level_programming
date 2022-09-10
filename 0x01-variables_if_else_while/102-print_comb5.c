3c7ff92375287abb783675381ba75db248ee9f75
Remise Ogenrwot <remiseogenrwot639@gmail.com>
2022-09-10 09:11:46 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main-Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int num1 = 0, num2;
+	
+	while (num1 <= 99)
+	{
+		num1 = num2;
+		while (num2 <= 99)
+		{
+			if (num1 != num2)
+			{
+				putchar((num1 / 10) + 48);
+				putchar((num1 % 10) + 48);
+				putchar(' ');
+				putchar((num2 / 10) + 48);
+				putchar((num2 % 10) + 48);
+				if (num1 != 98 || num2 != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++n2;
+		}
+		++n1;
+	}
+	putchar('\n');
+	return (0);
+}

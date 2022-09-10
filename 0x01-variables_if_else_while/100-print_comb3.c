0a0ee3d3805e5885b592fc213ef73de743c2e7e3
Remise Ogenrwot <remiseogenrwot639@gmail.com>
2022-09-10 07:45:19 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main-Entry point
@@ -8,11 +10,11 @@ int main(void)
 {
 	int number;
 
-	for (number = 0; number < 90; number++)
+	for (number = 0; number < 100; number++)
 	{
 		putchar((number / 10) + '0');
-		putchar((number % 10) + '1');
-		if (number != 89)
+		putchar((number % 10) + '0');
+		if (number != 99)
 		{
 			putchar(',');
 			putchar(' ');

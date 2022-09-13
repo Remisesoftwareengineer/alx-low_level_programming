02c364ff3fa73aaaee411629b87f493dea52e0e4
Remise Ogenrwot <remiseogenrwot639@gmail.com>
2022-09-13 16:07:16 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * main-Entry point
- * Description: times_table - Prints the 9 times table, starting with 0.
- * Return:0
+ * times_table - Prints the 9 times table, starting with 0 
  */
 void times_table(void)
 {

3d3b35f9222747ab23f2d62d4324bafa5bb60444
Remise Ogenrwot <remiseogenrwot639@gmail.com>
2022-09-19 14:53:46 -0700

    Number 7

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@
  * * int prototypes()
  *
  * */
+int _putchar(char k);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -19,6 +20,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *str);
-int _putchar(char c)
 
 #endif /*MAIN_H*/

d9240565d8ee8ebb3833ac3522e459731b96554e
Remise Ogenrwot <remiseogenrwot639@gmail.com>
2022-09-19 14:26:54 -0700

    Number 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,14 +9,14 @@
  */
 int main(void)
 {
-	char passwaord[84];
+	char password[84];
 	int index = 0, sum = 0, diff_half1, diff_half2;
 
 	srand(time(0));
 
 	while (sum < 2772)
 	{
-		password[index] = 33 + srand() % 94;
+		password[index] = 33 + rand() % 94;
 		sum += password[index++];
 	}
 	password[index] = '\0';
